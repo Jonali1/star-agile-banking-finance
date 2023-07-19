ab4adb817bd8f05968e4c34bb5ff8f2dd00c33b6
Jonali1 <130118044+Jonali1@users.noreply.github.com>
2023-07-20 00:53:04 +0530

    Add files via upload

--- terraform.tf
+++ terraform.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "Prod-Server" {
  ami = "ami-053b0d53c279acc90"
  instance_type = "t2.micro"
  availability_zone = "us-east-1a"
- key_name = "keypair"
+ key_name = "key"
  network_interface {
  device_index = 0
  network_interface_id = aws_network_interface.proj-ni.id
@@ -139,8 +139,9 @@ resource "aws_instance" "Prod-Server" {
      sudo apt-get update -y
      sudo apt install docker.io -y
      sudo systemctl enable docker
-     sudo docker run -itd -p 8084:8081 jonalitalukdar/banking1:1.0
+     sudo docker run -itd -p 8084:8081 jonalitalukdar/banking2:1.0
      sudo docker start $(docker ps -aq)
+     sudo docker run -itd -p 9100:9100 prom/node-exporter
  EOF
  tags = {
  Name = "Prod-Server"
